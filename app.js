e19f57b41819e4716d94cfb5d87f756350246206
Cesar Fuentes <alexmarticorena2@gmail.com>
2023-10-04 23:33:47 -0600

    Mi primer Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+new Vue({
+    el: '#app',
+    data: {
+        // Puedes agregar datos y propiedades necesarios aquí
+    },
+    methods: {
+        // Puedes definir métodos para manejar la interacción del usuario
+    }
+});
---
+++ app.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+      <SideMenu :isMenuOpen="isMenuOpen" @toggleMenu="toggleMenu" @navigate="changeView"/>
+      <div class="content-area">
+        <component :is="currentView" />
+        <button @click="toggleMenu">Mostrar/Ocultar Menú</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import SideMenu from './components/SideMenu.vue'
+  
+  export default {
+    components: {
+      SideMenu
+    },
+    data() {
+      return {
+        currentView: 'Home',
+        isMenuOpen: false
+      }
+    },
+    methods: {
+      changeView(view) {
+        this.currentView = view;
+        this.toggleMenu();
+      },
+      toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+      }
+    }
+  }
+  </script>
